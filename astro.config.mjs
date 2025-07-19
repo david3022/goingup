30964801bb0ee7c03cf6b1901f2577d37feed968
David3022 <davidafs36@gmail.com>
2025-07-18 18:58:57 -0600

    eliminated videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   adapter: netlify(),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwind()],
   }
 });
 
